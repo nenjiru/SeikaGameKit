962464998865498e340d27038ab722ae1779ce54
Minoru Nakanou <minoru.nakanou@gmail.com>
2023-05-08 00:36:17 +0900

    Get layout files from package

--- Editor/CustomLayout.cs
+++ Editor/CustomLayout.cs
@@ -1,22 +1,24 @@
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 namespace SeikaGameKit
 {
     public class CustomLayout : MonoBehaviour
     {
-        private const string LAYOUT_PATH = "Packages/SeikaGameKit/Resources/Layouts/SGKitLayout.wlt";
+        private const string LAYOUT_ASSET = "Packages/jp.digicre.seika-game-kit/Resources/Layouts/SGKitLayout.wlt";
 
         [MenuItem("Seika Game Kit/Load Custom Layout")]
         static void LoadCustomLayout()
         {
-            if (System.IO.File.Exists(LAYOUT_PATH))
+            string path = Path.GetFullPath(LAYOUT_ASSET);
+            if (System.IO.File.Exists(path))
             {
-                EditorUtility.LoadWindowLayout(LAYOUT_PATH);
+                EditorUtility.LoadWindowLayout(path);
             }
             else
             {
-                Debug.LogWarning("Layout not loaded. Layout file missing at: " + LAYOUT_PATH);
+                Debug.LogWarning("Layout not loaded. Layout file missing at: " + LAYOUT_ASSET);
             }
         }
     }
