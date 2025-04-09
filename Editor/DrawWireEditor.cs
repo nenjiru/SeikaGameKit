f58db8d96a7baf5ff0f35dac91889a393103f107
minoru nakanou <minoru.nakanou@gmail.com>
2025-04-10 04:38:10 +0900

    Camera Gizmo integrated into Draw Wire

--- Editor/DrawWireEditor.cs
+++ Editor/DrawWireEditor.cs
@@ -6,23 +6,23 @@ namespace SeikaGameKit.Helper
     [CustomEditor(typeof(DrawWire))]
     public class DrawWireEditor : BaseEditor
     {
-        DrawWire _target;
         SerializedProperty _enable;
-        SerializedProperty _color;
         SerializedProperty _type;
+        SerializedProperty _color;
         SerializedProperty _size;
         SerializedProperty _radius;
         SerializedProperty _height;
+        string[] _drawTypes;
 
         void OnEnable()
         {
-            _target = (DrawWire)target;
             _enable = serializedObject.FindProperty("_enable");
-            _color = serializedObject.FindProperty("_color");
             _type = serializedObject.FindProperty("_type");
+            _color = serializedObject.FindProperty("_color");
             _size = serializedObject.FindProperty("_size");
             _radius = serializedObject.FindProperty("_radius");
             _height = serializedObject.FindProperty("_height");
+            _drawTypes = System.Enum.GetNames(typeof(DrawWire.DrawType));
         }
 
         public override void OnInspectorGUI()
@@ -31,23 +31,27 @@ namespace SeikaGameKit.Helper
             base.ScriptField();
 
             EditorGUILayout.PropertyField(_enable);
-            EditorGUILayout.PropertyField(_color);
 
-            var type = (DrawWire.DrawType)EditorGUILayout.Popup("Type", _type.enumValueIndex, System.Enum.GetNames(typeof(DrawWire.DrawType)));
-            if (type == DrawWire.DrawType.Cube)
+            if (_enable.boolValue)
             {
-                EditorGUILayout.PropertyField(_size);
+                var type = (DrawWire.DrawType)EditorGUILayout.Popup("Type", _type.enumValueIndex, _drawTypes);
+                if (type == DrawWire.DrawType.Cube)
+                {
+                    EditorGUILayout.PropertyField(_size);
+                }
+                if (type == DrawWire.DrawType.Sphere)
+                {
+                    EditorGUILayout.PropertyField(_radius);
+                }
+                if (type == DrawWire.DrawType.Capsule)
+                {
+                    EditorGUILayout.PropertyField(_height);
+                    EditorGUILayout.PropertyField(_radius);
+                }
+                _type.enumValueIndex = (int)type;
+
+                EditorGUILayout.PropertyField(_color);
             }
-            if (type == DrawWire.DrawType.Sphere)
-            {
-                EditorGUILayout.PropertyField(_radius);
-            }
-            if (type == DrawWire.DrawType.Capsule)
-            {
-                EditorGUILayout.PropertyField(_height);
-                EditorGUILayout.PropertyField(_radius);
-            }
-            _type.enumValueIndex = (int)type;
 
             serializedObject.ApplyModifiedProperties();
         }
--- Runtime/Helper/DrawWire.cs
+++ Runtime/Helper/DrawWire.cs
@@ -3,28 +3,30 @@ using UnityEngine;
 namespace SeikaGameKit.Helper
 {
     /// <summary>
-    /// 領域をワイヤー描画
+    /// Drawing lines on game objects
     /// </summary>
     public class DrawWire : MonoBehaviour
     {
         #region DEFINITION
         public enum DrawType
         {
+            Camera,
+            Collider,
             Cube,
             Sphere,
             Capsule,
-            Collider,
         }
         #endregion
 
         #region VARIABLE
         [SerializeField, Tooltip("描画の有効化")] bool _enable = true;
-        [SerializeField, Tooltip("描画色")] Color _color = Color.green;
         [SerializeField, Tooltip("描画の形状")] DrawType _type = DrawType.Cube;
+        [SerializeField, Tooltip("描画色")] Color _color = Color.green;
         [SerializeField, Tooltip("領域を指定")] Vector3 _size = Vector3.one;
         [SerializeField, Tooltip("半径を指定")] float _radius = 0.5f;
         [SerializeField, Tooltip("高さを指定")] float _height = 2f;
-        Collider _collider;
+        Camera _camera = null;
+        Collider _collider = null;
         #endregion
 
 #if UNITY_EDITOR
@@ -41,17 +43,25 @@ namespace SeikaGameKit.Helper
             Gizmos.color = _color;
             Gizmos.matrix = transform.localToWorldMatrix;
 
-            if (_type == DrawType.Cube)
+            if (_type == DrawType.Camera)
+            {
+                if (_camera == null)
+                {
+                    _camera = GetComponent<Camera>();
+                }
+                DrawCameraFrustum(_camera, _color);
+            }
+            else if (_type == DrawType.Cube)
             {
                 Gizmos.DrawWireCube(Vector3.zero, _size);
             }
             else if (_type == DrawType.Sphere)
             {
-                Gizmos.DrawSphere(Vector3.zero, _radius);
+                Gizmos.DrawWireSphere(Vector3.zero, _radius);
             }
             else if (_type == DrawType.Capsule)
             {
-                DrawWire.DrawCapsuleGizmo(transform, _radius, _height, 1, _color);
+                DrawCapsuleGizmo(transform, _radius, _height, 1, _color);
             }
             else if (_type == DrawType.Collider)
             {
@@ -66,7 +76,7 @@ namespace SeikaGameKit.Helper
                         Gizmos.DrawWireCube(Vector3.zero + box.center, box.size);
                         break;
                     case CapsuleCollider capsule:
-                        DrawWire.DrawCapsuleGizmo(transform, capsule.radius, capsule.height, capsule.direction, _color);
+                        DrawCapsuleGizmo(transform, capsule.radius, capsule.height, capsule.direction, _color);
                         break;
                     case SphereCollider sphere:
                         Gizmos.DrawWireSphere(Vector3.zero + sphere.center, sphere.radius);
@@ -80,22 +90,45 @@ namespace SeikaGameKit.Helper
         #endregion
 
         #region PUBLIC_METHOD
-        public static void DrawCapsuleGizmo(Transform transform, float radius, float height, int direction, Color color = default)
-        {
-            DrawCapsuleGizmo(transform, radius, height, direction, Vector3.zero, color);
-        }
-
-        public static void DrawCapsuleGizmo(Transform transform, float radius, float height, Vector3 offset, Color color = default)
-        {
-            DrawCapsuleGizmo(transform, radius, height, 1, offset, color);
-        }
-        #endregion
-
-        #region PRIVATE_METHOD
         /// <summary>
-        /// Drawing capsule wires
+        /// Drawing camera frustum
         /// </summary>
-        private static void DrawCapsuleGizmo(Transform transform, float radius, float height, int direction, Vector3 offset, Color color = default)
+        public static void DrawCameraFrustum(Camera camera, Color color = default)
+        {
+            if (camera == null)
+            {
+                return;
+            }
+
+            float fov = camera.fieldOfView;
+            float size = camera.orthographicSize;
+            float max = camera.farClipPlane;
+            float min = camera.nearClipPlane;
+            float aspect = camera.aspect;
+
+            Color tempColor = Gizmos.color;
+            Gizmos.color = color == default ? Color.white : color;
+
+            Matrix4x4 tempMat = Gizmos.matrix;
+            Gizmos.matrix = Matrix4x4.TRS(camera.transform.position, camera.transform.rotation, new Vector3(aspect, 1.0f, 1.0f));
+
+            if (camera.orthographic)
+            {
+                Gizmos.DrawWireCube(Vector3.forward * ((max - min) / 2.0f) + Vector3.forward * min, new Vector3(size * 2.0f * aspect, size * 2.0f, max - min));
+            }
+            else
+            {
+                Gizmos.DrawFrustum(Vector3.zero, fov, max, min, 1.0f);
+            }
+
+            Gizmos.color = tempColor;
+            Gizmos.matrix = tempMat;
+        }
+
+        /// <summary>
+        /// Draw capsule gizmo
+        /// </summary>
+        private static void DrawCapsuleGizmo(Transform transform, float radius, float height, int direction, Color color = default)
         {
             Color tmpColor = UnityEditor.Handles.color;
             UnityEditor.Handles.color = color == default ? Color.white : color;
@@ -106,7 +139,7 @@ namespace SeikaGameKit.Helper
                 case 2: rot *= Quaternion.Euler(90, 0, 0); break;
             }
 
-            Matrix4x4 matrix = Matrix4x4.TRS(transform.position + offset, rot, transform.lossyScale);
+            Matrix4x4 matrix = Matrix4x4.TRS(transform.position, rot, transform.lossyScale);
             using (new UnityEditor.Handles.DrawingScope(matrix))
             {
                 float halfHeight = (height - (radius * 2)) / 2;
